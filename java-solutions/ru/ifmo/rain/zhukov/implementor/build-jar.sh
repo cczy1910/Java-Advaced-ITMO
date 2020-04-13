80781538dcf4506bf6842a12ed1a701536cd06d0
cczy1910 <z240956z@yandex.ru>
2020-04-13 20:07:07 +0300

    build-jar reworked.

--- java-solutions/ru/ifmo/rain/zhukov/implementor/build-jar.sh
+++ java-solutions/ru/ifmo/rain/zhukov/implementor/build-jar.sh
@@ -1,10 +1,8 @@
 #!/usr/bin/env bash
 
-INIT_PATH=${PWD}
+WD="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd)"
 
-cd "$(dirname $0)"/../../../../../../../
-
-ROOT=${PWD}
+ROOT=${WD}/../../../../../../../
 
 BASIC_PATH=${ROOT}/java-advanced-2020
 SOLUTION_PATH=${ROOT}/java-advanced-2020-solutions
@@ -19,8 +17,6 @@ JAR_PATH=${SOLUTION_PATH}/java-solutions/${MODULE_PATH}
 
 rm -rf ${OUT_PATH}
 
-javac --module-path ${REQ_PATH} ${SRC_PATH}/module-info.java ${SRC_PATH}/${MODULE_PATH}/*.java -d ${OUT_PATH}
+javac --module-path ${REQ_PATH} -d ${OUT_PATH} ${SRC_PATH}/module-info.java ${WD}/*.java
 
 jar -c --file ${JAR_PATH}/_implementor.jar --main-class=${MODULE_NAME}.implementor.JarImplementor --module-path ${REQ_PATH} -C ${OUT_PATH} .
-
-cd ${INIT_PATH}
